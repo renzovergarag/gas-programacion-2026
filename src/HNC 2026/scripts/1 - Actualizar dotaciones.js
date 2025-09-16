e88196273336d01741b2b9d8dbfbb7a831cdf1df
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-09-16 17:06:34 -0300

    in progress: migracion automatica de dotaciones en proceso

---
+++ src/HNC 2026/scripts/1 - Actualizar dotaciones.js
@@ -0,0 +1,18 @@
+function migrarDotaciones() {
+    const ID_HOJA_ESTABLECIMIENTOS = "1xVWBfmaSKHajoiw95Vg9Z1KJevPRm_-Ll4XIrYc3cmU";
+    let establecimientos = obtenerListaEstablecimientos(ID_HOJA_ESTABLECIMIENTOS);
+
+    // for (var i = 1; i < establecimientos.length; i++) {
+    for (var i = 1; i < 2; i++) {
+        let hnc = SpreadsheetApp.openByUrl(establecimientos[i][5]);
+        let programacion = SpreadsheetApp.openByUrl(establecimientos[i][3]);
+        migrarInformacionDotacion(hnc, programacion);
+        Logger.log(establecimientos[i][0] + " ha sido Migrado!");
+    }
+}
+
+function migrarInformacionDotacion(origen, destino) {
+    let datosOrigen;
+    datosOrigen = origen.getSheetByName("Horas_FUNC").getRange("B7:B199").getValues();
+    destino.getSheetByName("DOTACION").getRange("A3:A195").setValues(datosOrigen);
+}

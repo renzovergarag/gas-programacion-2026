6b03a2fc182f7d32bf8eef13f33445436a8bc24b
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-09-17 11:35:24 -0300

    cambios pendients

--- src/HNC 2026/scripts/1 - Actualizar dotaciones.js
+++ src/HNC 2026/scripts/1 - Actualizar dotaciones.js
@@ -5,10 +5,18 @@ function migrarDotaciones() {
     // for (var i = 1; i < establecimientos.length; i++) {
     for (var i = 1; i < 2; i++) {
         let hnc = SpreadsheetApp.openByUrl(establecimientos[i][5]);
+        let refHnc = SpreadsheetApp.openByUrl(establecimientos[i][6]);
         let programacion = SpreadsheetApp.openByUrl(establecimientos[i][3]);
-        migrarInformacionDotacion(hnc, programacion);
+        migrarInformacionListaEstamentos(refHnc, programacion);
+        //migrarInformacionDotacion(hnc, programacion);
         Logger.log(establecimientos[i][0] + " ha sido Migrado!");
     }
+}
+
+function migrarInformacionListaEstamentos(origen, destino) {
+    let datosOrigen;
+    datosOrigen = origen.getSheetByName("Horas_FUNC").getRange("B7:B199").getValues();
+    destino.getSheetByName("DOTACION").getRange("A3:A195").setValues(datosOrigen);
 }
 
 function migrarInformacionDotacion(origen, destino) {

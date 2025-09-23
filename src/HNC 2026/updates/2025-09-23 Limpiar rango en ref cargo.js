dc84a55e6b2a6bf4bb5521ec8763741f7107bc7b
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-09-23 10:18:48 -0300

    ready: se agrega funcion para limpiar los valores de un rango especifico en hnc ref

---
+++ src/HNC 2026/updates/2025-09-23 Limpiar rango en ref cargo.js
@@ -0,0 +1,5 @@
+function limpiarRangoEnFilaTons() {
+    const hoja = "REF_CARGO"; // Reemplaza con el nombre de tu hoja
+    const rangoString = "E16:AU16"; // Reemplaza con el rango que deseas limpiar
+    limpiarRangoEnBdHnc(hoja, rangoString);
+}

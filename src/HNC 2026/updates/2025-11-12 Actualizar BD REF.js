ad86c72770c5d58316831285f0f83a1c34e1570d
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-11-12 17:14:14 -0300

    in progress: se mejora la documentacion de la funcion actualizarBdRefHNC2026

--- src/HNC 2026/updates/2025-11-12 Actualizar BD REF.js
+++ src/HNC 2026/updates/2025-11-12 Actualizar BD REF.js
@@ -1,15 +1,15 @@
-function actualizarReunionesHNC2026(nuevosNombres) {
+function actualizarBdRefHNC2026() {
     const ID_PLANTILLA_ORIGEN = "1kP2bL9OXpIWDUAJQldOYBRhgpalJ2Kc8zi7XCCsP1rc";
     const ID_HOJA_ESTABLECIMIENTOS = "1xVWBfmaSKHajoiw95Vg9Z1KJevPRm_-Ll4XIrYc3cmU";
 
     let tablaEstablecimientosReuniones = obtenerListaEstablecimientos(ID_HOJA_ESTABLECIMIENTOS);
     let origen = SpreadsheetApp.openById(ID_PLANTILLA_ORIGEN);
 
-    // Actualizar los nombres de los establecimientos
+    //* Actualizar la bd de referencia en cada establecimiento (reuniones, cargos de gestión y derechos funcionarios)
     for (let i = 1; i < tablaEstablecimientosReuniones.length; i++) {
         let destino = SpreadsheetApp.openByUrl(tablaEstablecimientosReuniones[i][6]);
         MigrarValoresHNC(origen, destino);
-        Logger.log(tablaEstablecimientosReuniones[i][0] + " ha sido Migrado!");
+        Logger.log("La información del " + tablaEstablecimientosReuniones[i][0] + " ha sido actualizada!");
     }
 }
 
@@ -20,7 +20,7 @@ function MigrarValoresHNC(origen, destino) {
     //console.log(datosOrigen);
     destino.getSheetByName("BD_REF_REUNIONES").getRange("A2:D234").setValues(datosOrigen);
 
-     //Parte 1.2
+    //Parte 1.2
     datosOrigen = origen.getSheetByName("BD_REF_REUNIONES").getRange("F2:M234").getValues();
     //console.log(datosOrigen);
     destino.getSheetByName("BD_REF_REUNIONES").getRange("F2:M234").setValues(datosOrigen);

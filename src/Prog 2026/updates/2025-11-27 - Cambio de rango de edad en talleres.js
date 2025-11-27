4d996c70e96282c51a20a8ee3ea3a51fcb5f722d
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-11-27 17:32:25 -0300

    ready: actualizacion de los rangos etarios de talleres de lenguaje y motor

---
+++ src/Prog 2026/updates/2025-11-27 - Cambio de rango de edad en talleres.js
@@ -0,0 +1,14 @@
+function actualizacionRangoEdadTaller20251127() {
+    let hoja = "PROG A. GRUPALES"; // Reemplaza con el nombre de tu hoja
+    let celda;
+    let valor;
+
+    //TALLER DEL LENGUAJE
+    celda = "E26"; // Reemplaza con la celda que deseas actualizar
+    valor = "0 AÑOS"; // Reemplaza con el valor que deseas establecer
+    actualizarValorEnBdProg(hoja, celda, valor);
+    //TALLER MOTOR
+    celda = "E27"; // Reemplaza con la celda que deseas actualizar
+    valor = "0 AÑOS"; // Reemplaza con el valor que deseas establecer
+    actualizarValorEnBdProg(hoja, celda, valor);
+}

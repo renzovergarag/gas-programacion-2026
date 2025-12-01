6550fe25b472cab2ede55f0dc7e060f276c3ab9e
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-12-01 17:05:13 -0300

    ready: actualizacion de estamento de baciloscopia

---
+++ src/Prog 2026/updates/2025-12-01 - Cambiar estamento baciloscopia.js
@@ -0,0 +1,10 @@
+function actualizarEstamentoBaciloscopia20251201() {
+    const hoja = "PROG 2026"; // Reemplaza con el nombre de tu hoja
+    let celda;
+    let valor;
+
+    //FILA GENERACIÓN DE CASOS
+    celda = "E501"; // Reemplaza con la celda que deseas actualizar
+    valor = "TECNICO EN ENFERMERIA"; // Reemplaza con el valor que deseas establecer
+    actualizarValorEnBdProg(hoja, celda, valor);
+}

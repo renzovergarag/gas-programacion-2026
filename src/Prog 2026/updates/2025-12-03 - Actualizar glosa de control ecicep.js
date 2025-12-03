ed2b7813287f300fd49a3a4d9dfe1ff13b9b7060
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-12-03 09:53:28 -0300

    ready: actualizacion de la glosa de agenda del control ecicep. Se elimina un espacio invisible

---
+++ src/Prog 2026/updates/2025-12-03 - Actualizar glosa de control ecicep.js
@@ -0,0 +1,115 @@
+function actualizarGlosaControlEcicep20251203() {
+    const hoja = "PROG ECICEP"; // Reemplaza con el nombre de tu hoja
+    let celda;
+    let valor;
+
+    //C13:C15
+    celda = "C13"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD"; // Reemplaza con el valor que deseas establecer
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C14"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C15"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    //C46:C69
+    celda = "C46"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD"; // Reemplaza con el valor que deseas establecer
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C47"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C48"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C49"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C50"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C51"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C52"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C53"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C54"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C55"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C56"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C57"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C58"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C59"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C60"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C61"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C62"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C63"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C64"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C65"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C66"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C67"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C68"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+
+    celda = "C69"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD";
+    actualizarValorEnBdProg(hoja, celda, valor);
+}

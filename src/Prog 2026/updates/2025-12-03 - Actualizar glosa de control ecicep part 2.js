bdcd7e8db7aabc441d6988f83ca2acd4274e47ce
Renzo Vergara <rvergara@cmvalparaiso.cl>
2025-12-03 10:00:06 -0300

    ready: actualizacion de la glosa de agenda del control ecicep. Se elimina un espacio invisible

---
+++ src/Prog 2026/updates/2025-12-03 - Actualizar glosa de control ecicep part 2.js
@@ -0,0 +1,9 @@
+function actualizarGlosaControlEcicep220251203() {
+    const hoja = "PROG 2026"; // Reemplaza con el nombre de tu hoja
+    let celda;
+    let valor;
+
+    celda = "D55"; // Reemplaza con la celda que deseas actualizar
+    valor = "CONTROL INTEGRAL MULTIMORBILIDAD"; // Reemplaza con el valor que deseas establecer
+    actualizarValorEnBdProg(hoja, celda, valor);
+}
